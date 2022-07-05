7f83ba4f0a5d077bc7fcbfc6bdf677857f790c45
Curtis W. DeGidio <curtis.degidio@gmail.com>
2022-07-05 00:46:09 -0500

    Added propeties and profile

--- src/main/java/com/cwdegidio/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/cwdegidio/myfancypdfinvoices/service/InvoiceService.java
@@ -2,6 +2,7 @@ package com.cwdegidio.myfancypdfinvoices.service;
 
 import com.cwdegidio.myfancypdfinvoices.model.Invoice;
 import com.cwdegidio.myfancypdfinvoices.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,10 +14,13 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class InvoiceService {
 
     private final UserService userService;
+
+    private final String cdnUrl;
     List<Invoice> invoices = new CopyOnWriteArrayList<>();
 
-    public InvoiceService(UserService userService) {
+    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
+        this.cdnUrl = cdnUrl;
     }
 
     public List<Invoice> findAll() {
@@ -31,7 +35,7 @@ public class InvoiceService {
         }
 
         // TODO create pdf creation and storing it on the network server
-        Invoice invoice =  new Invoice(userId, amount, "http://www.aficanu.edu/images/default/sample.pdf");
+        Invoice invoice =  new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
         invoices.add(invoice);
         return invoice;
     }
