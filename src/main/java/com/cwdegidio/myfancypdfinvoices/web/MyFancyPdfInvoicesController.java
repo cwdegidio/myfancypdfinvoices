211bdc8ece44b9c55cfee2253bda597645039f4f
Curtis W. DeGidio <curtis.degidio@gmail.com>
2022-07-08 02:29:04 -0500

    Created REST endpoing using Spring MVC annotations

--- src/main/java/com/cwdegidio/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
+++ src/main/java/com/cwdegidio/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -1,15 +1,31 @@
 package com.cwdegidio.myfancypdfinvoices.web;
 
-import org.springframework.stereotype.Controller;
+import com.cwdegidio.myfancypdfinvoices.model.Invoice;
+import com.cwdegidio.myfancypdfinvoices.service.InvoiceService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class MyFancyPdfInvoicesController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String index() {
-        return "Hello, World!";
+    private final InvoiceService invoiceService;
+
+    public MyFancyPdfInvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    // @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<Invoice> index() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping("/invoices")
+    public Invoice createInvoice(@RequestParam("user_id") String userId, @RequestParam Integer amount) {
+        return invoiceService.create(userId, amount);
     }
 }
