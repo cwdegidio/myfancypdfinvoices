5f9db5bb2b92b8ae02133e6d94be9145afeae2b8
Curtis W. DeGidio <curtis.degidio@gmail.com>
2022-07-08 02:12:23 -0500

    Created initial Spring MVC setup.

---
+++ src/main/java/com/cwdegidio/myfancypdfinvoices/web/MyFancyPdfInvoicesController.java
@@ -0,0 +1,15 @@
+package com.cwdegidio.myfancypdfinvoices.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFancyPdfInvoicesController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello, World!";
+    }
+}
